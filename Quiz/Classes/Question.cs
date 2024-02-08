329495f7e72414e42e31522d5698c101ff661e05
kremeda21 <daniel.kremen@email.cz>
2024-02-08 15:10:56 +0100

    Created question class

---
+++ Quiz/Classes/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Classes
+{
+    internal class Question
+    {
+        public int CorrectAnswer { get; set; }
+        public string Equation { get; set; }
+        public int[] Answers { get; set; }
+        public char Sign {  get; set; }
+
+        public Question()
+        {
+
+        }
+    }
+}
